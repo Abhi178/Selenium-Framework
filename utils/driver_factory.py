34776963b926b48a44bafdb00ad7f226f4f43091
baburaj <abhishek.baburaj@t-systems.com>
2026-07-08 12:47:29 +0530

    added github actions workflow

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -1,14 +1,36 @@
+import os
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 
 def get_driver():
 
-    service = Service(
-        r"C:\Pytest_Projects\selenium_framework\drivers\chromedriver.exe"
-    )
+    options = Options()
 
-    driver = webdriver.Chrome(service=service)
+    if os.getenv("HEADLESS", "false").lower() == "true":
+
+        options.add_argument("--headless=new")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+
+        # CI Mode
+        driver = webdriver.Chrome(
+            options=options
+        )
+
+    else:
+
+        # Local Mode
+        service = Service(
+            r"C:\Pytest_Projects\selenium_framework\drivers\chromedriver.exe"
+        )
+
+        driver = webdriver.Chrome(
+            service=service,
+            options=options
+        )
 
     driver.maximize_window()
 
